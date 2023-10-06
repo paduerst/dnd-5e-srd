a02043e6e57d114cc89639306f70b7faec56d8b5
Peter Duerst <peterad12@gmail.com>
2023-10-06 11:55:26 -0400

    WIP spells processed into JSON (#1)
    * WIP Python script to process spells
    
    * WIP spell processing: name, level, school
    
    * WIP spell processing: defined main()
    
    * WIP spells processing: exporting to spells.json
    
    * WIP testing of spells.json
    
    * Spell testing now up-to-date with spells.json
    
    * Added castingTime to spells.json
    
    * Confirmed shield castingTime exception only affects it
    
    * Improved testSpellCastingTime()
    
    * Added range to spells.json + updated testing for reactions
    
    * Cleanup before PR

---
+++ process-spells-from-export.py
@@ -0,0 +1,156 @@
+import json
+import os
+import re
+from typing import TypedDict
+
+from bs4 import BeautifulSoup
+
+normalizationRegexDash = re.compile('[ \/]')
+normalizationRegexEmpty = re.compile('[^a-z\-]')
+
+newlineRegex = re.compile('[\\n]')
+multispaceRegex = re.compile(' {2,}')
+
+allDashRegex = re.compile(r'[\u00ad\u2010\u2011]')
+multiDashRegex = re.compile(r'\-{2,}')
+
+abs_path_to_repo = os.path.dirname(os.path.abspath(__file__))
+
+rel_path_to_parts_folder = os.path.join(
+    "exported-from-adobe", "SRD5.1-CCBY4.0_License_live links_files")
+abs_path_to_parts_folder = os.path.join(
+    abs_path_to_repo, rel_path_to_parts_folder)
+
+file_name_for_spell_descriptions = "part646.htm"
+path_for_spell_descriptions = os.path.join(
+    abs_path_to_parts_folder, file_name_for_spell_descriptions)
+
+
+def normalizeString(string: str):
+    string_with_dashes = normalizationRegexDash.sub(
+        '-', string.lower().strip())
+    return normalizationRegexEmpty.sub('', string_with_dashes)
+
+
+class SpellLink:
+    def __init__(self, soup_link):
+        self.name = soup_link.string
+        self.id = normalizeString(self.name)
+        self.href = soup_link.get('href')
+        self.subLink = None
+
+    def addSubLink(self, soup_link):
+        self.subLink = soup_link.get('href')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"({self.name}, {self.id}, {self.href}, {self.subLink})"
+
+
+class SpellInfo(TypedDict):
+    name: str
+    level: int
+    school: str
+    castingTime: str
+    ritual: bool
+    range: str
+
+
+def getSpellInfo(link: SpellLink) -> SpellInfo:
+    output: SpellInfo = {}
+    output["name"] = link.name
+
+    # soup1 always contains the level, school, casting time, and range
+    path_to_part1 = os.path.join(abs_path_to_parts_folder, link.href)
+    with open(path_to_part1) as fp:
+        soup1 = BeautifulSoup(fp, 'lxml')
+    # print(soup1.prettify())
+
+    subheading_tag = soup1.select_one('.s12')
+    if subheading_tag == None:
+        raise RuntimeError(
+            f"Unable to find subheading_tag for spell {link}")
+    subheading = subheading_tag.string.strip()
+    subheading_arr = subheading.split(" ")
+    is_ritual = False
+    if subheading_arr[-1] == "(ritual)":
+        is_ritual = True
+        subheading_arr.pop()
+    if subheading[0].isdigit():
+        output["level"] = int(subheading[0])
+        output["school"] = subheading_arr[-1].capitalize()
+    else:
+        output["level"] = 0
+        output["school"] = subheading_arr[0]
+
+    strings1 = list(soup1.stripped_strings)
+    casting_time_index = strings1.index("Casting Time:")
+    spell_range_index = strings1.index("Range:")
+    casting_time = ' '.join(
+        strings1[(casting_time_index + 1):spell_range_index])
+    casting_time = newlineRegex.sub(' ', casting_time)
+    casting_time = multispaceRegex.sub(' ', casting_time)
+    output["castingTime"] = casting_time.strip()
+
+    # ritual bool should come right after casting time
+    output["ritual"] = is_ritual
+
+    spell_range = strings1[spell_range_index + 1]
+    spell_range = allDashRegex.sub('-', spell_range)
+    spell_range = multiDashRegex.sub('-', spell_range)
+    output["range"] = spell_range
+
+    # soup2 contains the rest, starting with components
+    path_to_part2 = path_to_part1
+    if link.subLink != None:
+        path_to_part2 = os.path.join(abs_path_to_parts_folder, link.subLink)
+    with open(path_to_part2) as fp:
+        soup2 = BeautifulSoup(fp, 'lxml')
+    # print(soup2.prettify())
+
+    return output
+
+
+def saveSpells(spells: list[SpellLink]):
+    data = {}
+    for link in spells:
+        data[link.id] = getSpellInfo(link)
+    output_path = os.path.join(
+        abs_path_to_repo, 'spells', 'spells.json')
+    with open(output_path, 'w') as fp:
+        json.dump(data, fp, indent=2)
+
+
+def main():
+    with open(path_for_spell_descriptions) as fp:
+        soup = BeautifulSoup(fp, 'lxml')
+
+    srd_spell_links: list[SpellLink] = []
+    for link in soup.find_all('a'):
+        if link.get('class') == None:
+            continue
+        elif link.get('class') == ['toc0']:
+            srd_spell_links.append(SpellLink(link))
+        elif link.get('class') == ['toc1']:
+            srd_spell_links[-1].addSubLink(link)
+        else:
+            continue
+
+    DEBUG_SPELL_PROCESSING = False
+    if DEBUG_SPELL_PROCESSING:
+        print(getSpellInfo(srd_spell_links[0]))
+        print(getSpellInfo(srd_spell_links[1]))
+        print(getSpellInfo(srd_spell_links[10]))
+        print(getSpellInfo(srd_spell_links[33]))
+        print(getSpellInfo(srd_spell_links[64]))
+        print(getSpellInfo(srd_spell_links[260]))
+    else:
+        saveSpells(srd_spell_links)
+
+    print("Spell processing complete!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ spells/test-spells.py
@@ -0,0 +1,184 @@
+import json
+import re
+
+SPELL_NAME_MIN_LENGTH = 3
+SPELL_LEVEL_VALUES = range(10)
+SPELL_SCHOOL_VALUES = [
+    'Abjuration',
+    'Conjuration',
+    'Divination',
+    'Enchantment',
+    'Evocation',
+    'Illusion',
+    'Necromancy',
+    'Transmutation'
+]
+SPELL_CASTING_TIME_VALUES = [
+    '1 action',
+    '1 bonus action',
+    '1 minute',
+    '10 minutes',
+    '1 hour',
+    '8 hours',
+    '12 hours',
+    '24 hours',
+    '1 action or 8 hours',
+    '1 reaction, which you take when you see a creature within 60 feet of you casting a spell',
+    '1 reaction, which you take when you or a creature within 60 feet of you falls',
+    '1 reaction, which you take in response to being damaged by a creature within 60 feet of you that you can see',
+    '1 reaction, which you take when you are hit by an attack or targeted by the magic missile spell',
+]
+SPELL_RANGE_VALUES = [
+    'Self',
+    'Touch',
+    'Sight',
+    'Unlimited',
+    'Special',
+    '5 feet',
+    '10 feet',
+    '30 feet',
+    '60 feet',
+    '90 feet',
+    '100 feet',
+    '120 feet',
+    '150 feet',
+    '300 feet',
+    '500 feet',
+    '1 mile',
+    '500 miles',
+    'Self (10-foot radius)',
+    'Self (15-foot radius)',
+    'Self (30-foot radius)',
+    'Self (5-mile radius)',
+    'Self (10-foot-radius sphere)',
+    'Self (10-foot-radius hemisphere)',
+    'Self (15-foot cone)',
+    'Self (30-foot cone)',
+    'Self (60-foot cone)',
+    'Self (60-foot line)',
+    'Self (100-foot line)',
+    'Self (15-foot cube)',
+]
+
+
+def testSpellName(spell: dict) -> bool:
+    name = spell.get("name")
+    if name == None:
+        print(f"Failure: no name found in {spell}")
+        return False
+    elif not isinstance(name, str):
+        print(f"Failure: name not string in {spell}")
+        return False
+    elif len(name) < SPELL_NAME_MIN_LENGTH:
+        print(f"Failure: name too short in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpellLevel(spell: dict) -> bool:
+    level = spell.get("level")
+    if level == None:
+        print(f"Failure: no level found in {spell}")
+        return False
+    elif not isinstance(level, int):
+        print(f"Failure: level not int in {spell}")
+        return False
+    elif not level in SPELL_LEVEL_VALUES:
+        print(f"Failure: level not valid in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpellSchool(spell: dict) -> bool:
+    school = spell.get("school")
+    if school == None:
+        print(f"Failure: no school found in {spell}")
+        return False
+    elif not isinstance(school, str):
+        print(f"Failure: school not str in {spell}")
+        return False
+    elif not school in SPELL_SCHOOL_VALUES:
+        print(f"Failure: school not valid in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpellCastingTime(spell: dict) -> bool:
+    castingTime = spell.get("castingTime")
+    if castingTime == None:
+        print(f"Failure: no castingTime found in {spell}")
+        return False
+    elif not isinstance(castingTime, str):
+        print(f"Failure: castingTime not str in {spell}")
+        return False
+    elif (not castingTime in SPELL_CASTING_TIME_VALUES):
+        print(f"Failure: castingTime not valid in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpellRitual(spell: dict) -> bool:
+    ritual = spell.get("ritual")
+    if ritual == None:
+        print(f"Failure: no ritual found in {spell}")
+        return False
+    elif not isinstance(ritual, bool):
+        print(f"Failure: ritual not bool in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpellRange(spell: dict) -> bool:
+    spellRange = spell.get("range")
+    if spellRange == None:
+        print(f"Failure: no range found in {spell}")
+        return False
+    elif not isinstance(spellRange, str):
+        print(f"Failure: range not str in {spell}")
+        return False
+    elif (not spellRange in SPELL_RANGE_VALUES):
+        print(f"Failure: range not valid in {spell}")
+        return False
+    else:
+        return True
+
+
+def testSpell(spell: dict) -> bool:
+    output = True
+
+    output = output and testSpellName(spell)
+    output = output and testSpellLevel(spell)
+    output = output and testSpellSchool(spell)
+    output = output and testSpellCastingTime(spell)
+    output = output and testSpellRitual(spell)
+    output = output and testSpellRange(spell)
+
+    return output
+
+
+def main():
+    spells_path = "spells.json"
+    with open(spells_path) as fp:
+        spells = json.load(fp)
+
+    all_passed = True
+    failed_count = 0
+    for key in spells:
+        if not testSpell(spells[key]):
+            all_passed = False
+            failed_count = failed_count + 1
+            print(f"Failed for key: {key}")
+
+    if all_passed:
+        print("Testing complete: all spells passed!")
+    else:
+        print(f"Testing complete: {failed_count} spell(s) failed.")
+
+
+if __name__ == "__main__":
+    main()
