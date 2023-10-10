3a50acc09c8c150b76c874b5d3f4bdd58cc8efb6
Peter Duerst <peterad12@gmail.com>
2023-10-10 19:32:09 -0400

    Collated spell descriptions into JSON

---
+++ spells/collate-spell-descriptions.py
@@ -0,0 +1,41 @@
+import json
+import os
+import re
+
+newlineRegex = re.compile(r'[\n]')
+multispaceRegex = re.compile(r'\s{2,}')
+
+abs_path_to_this_folder = os.path.dirname(os.path.abspath(__file__))
+path_to_descriptions_folder = os.path.join(
+    abs_path_to_this_folder, "descriptions")
+path_to_output_file = os.path.join(
+    abs_path_to_this_folder, "spell-descriptions.json")
+
+
+def cleanSpellDescription(description: str):
+    output = description.strip()
+    output = newlineRegex.sub('', output)
+    output = multispaceRegex.sub(' ', output)
+    output = output.replace("<p> ", "<p>")
+    return output
+
+
+def main():
+    output = {}
+    filenames = os.listdir(path_to_descriptions_folder)
+    for filename in filenames:
+        spell_id = filename.split(".")[0]
+        spell_path = os.path.join(path_to_descriptions_folder, filename)
+        with open(spell_path, 'r') as fp:
+            spell_description = fp.read()
+        output[spell_id] = cleanSpellDescription(spell_description)
+
+    with open(path_to_output_file, 'w') as fp:
+        json.dump(output, fp, indent=2, sort_keys=True)
+        fp.write('\n')
+
+    print("Finished collating spell descriptions!")
+
+
+if __name__ == "__main__":
+    main()
